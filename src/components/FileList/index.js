1ad1066e40d88ee75eaacd9cf82474c4ee99b162
juan-patrick <juan-p.andrade@outlook.com>
2020-11-08 09:04:37 -0300

    Add circular progress and update styles and guide line

--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { Container, ImageInformation, Image } from './styles';
+import { CircularProgressbar } from 'react-circular-progressbar';
 
-function FileList() {
+import { Container, FileInfo, Preview } from './styles';
+
+import { ThemeContext } from 'styled-components';
+
+function FileList(props) {
+  const themeContext = useContext(ThemeContext);
+
   return (
     <Container>
+      <h4>Imagens carregadas</h4>
       <ul>
         <li>
-          <ImageInformation>
-            <Image
+          <FileInfo>
+            <Preview
               src="https://scontent.fgru10-1.fna.fbcdn.net/v/t1.0-9/76661762_1398068277035261_2106777747559809024_o.jpg?_nc_cat=107&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeEQ-IE8dCMWv_p0qobPaqG-XchR-_7t3_BdyFH7_u3f8LjMzrAg9AcBWgKqdg-65lGUXrAsT0EWQ_AoY9Ua0K46&_nc_ohc=1vKljZXYpOkAX-FBvM-&_nc_ht=scontent.fgru10-1.fna&oh=aad8029a2e1c279376b6b368974f11c2&oe=5FCBA996"
-              alt="upload"
+              alt="preview"
             />
 
             <div>
               <strong>profile.png</strong>
               <span>2.4 mb</span>
             </div>
-          </ImageInformation>
+          </FileInfo>
+          <div>
+            <CircularProgressbar
+              styles={{
+                root: { width: 35 },
+                path: {
+                  stroke: themeContext.colors.primary,
+                  strokeLinecap: 'round',
+                },
+                trail: {
+                  stroke: themeContext.colors.subtext,
+                },
+              }}
+              strokeWidth={10}
+              value={50}
+            />
+          </div>
         </li>
       </ul>
     </Container>
--- src/components/FileList/styles.js
+++ src/components/FileList/styles.js
@@ -2,8 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
+  flex-direction: column;
   height: 100%;
   width: 100%;
+
+  & h4 {
+    font-size: 1.2em;
+    color: ${(props) => props.theme.colors.subtext};
+    margin-bottom: 10px;
+  }
 
   & ul {
     display: flex;
@@ -12,16 +19,22 @@ export const Container = styled.section`
     width: 100%;
     list-style: none;
   }
+
+  & ul li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
-export const Image = styled.img`
+export const Preview = styled.img`
   width: 50px;
   height: 50px;
   border-radius: 4px;
   object-fit: cover;
 `;
 
-export const ImageInformation = styled.div`
+export const FileInfo = styled.div`
   display: flex;
   width: 100%;
 
@@ -29,7 +42,7 @@ export const ImageInformation = styled.div`
     margin-top: 5px;
     color: ${(props) => props.theme.colors.subtext};
     font-weight: 600;
-    font-size: 0.8em;
+    font-size: 0.85em;
   }
 
   & div {
