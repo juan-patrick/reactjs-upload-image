448dcb8e0b57cf5176791eb3db66fb11caf2c33a
Juan Patrick <juan-p.andrade@outlook.com>
2020-11-10 20:26:52 -0300

    Bug fix: #1

--- src/providers/UploadProvider.js
+++ src/providers/UploadProvider.js
@@ -1,16 +1,27 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 import { uniqueId } from 'lodash';
 
 import filesize from 'filesize';
+
+import api from '../services/api';
 
 export const UploadContext = createContext({});
 
 export default function UploadProvider({ children }) {
   const [uploadedFiles, setUploadedFiles] = useState([]);
 
-  const handleUpload = (files) => {
-    const uploadFiles = files.map((file) => ({
+  const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    if (uploading) {
+      uploadedFiles.forEach(processUpload);
+      setUploading(false);
+    }
+  }, [uploading]);
+
+  function handleUpload(files) {
+    const filesToUpload = files.map((file) => ({
       file,
       id: uniqueId(),
       name: file.name,
@@ -22,8 +33,50 @@ export default function UploadProvider({ children }) {
       url: null,
     }));
 
-    setUploadedFiles([...uploadedFiles, ...uploadFiles]);
-  };
+    setUploadedFiles(uploadedFiles.concat(filesToUpload));
+
+    setUploading(true);
+  }
+
+  function updateFile(id, data) {
+    setUploadedFiles(
+      uploadedFiles.map((uploadedFile) => {
+        return id === uploadedFile.id
+          ? { ...uploadedFile, ...data }
+          : uploadedFile;
+      })
+    );
+  }
+
+  function processUpload(uploadedFile) {
+    const data = new FormData();
+
+    data.append('file', uploadedFile.file, uploadedFile.name);
+
+    api
+      .post('/posts', data, {
+        onUploadProgress: (e) => {
+          const progress = parseInt(Math.round((e.loaded * 100) / e.total));
+
+          updateFile(uploadedFile.id, {
+            progress,
+          });
+        },
+      })
+      .then(({ data }) => {
+        updateFile(uploadedFile.id, {
+          uploaded: true,
+          id: data._id,
+          url: data.url,
+        });
+      })
+      .catch(() => {
+        updateFile(uploadedFile.id, {
+          error: true,
+        });
+      });
+  }
+
   return (
     <UploadContext.Provider
       value={{ handleUpload, uploadedFiles, setUploadedFiles }}
